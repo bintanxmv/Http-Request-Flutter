b75885ff8f767c5e84590be648bbd6ab49cb1392
Bintang Satria Mulya Budi <bintangsatriamulya33@gmail.com>
2024-04-05 18:09:48 +0700

    Your commit message

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:http_request_xir3/pages/movie_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MovieList();
+  }
+}
+
+
+
---
+++ lib/pages/movie_list.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:http_request_xir3/models/movie.dart';
+import 'package:http_request_xir3/pages/movie_detail.dart';
+import 'package:http_request_xir3/service/http_service.dart';
+
+class MovieList extends StatefulWidget {
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  int moviesCount = 0;
+  List<Movie> movies = [];
+  HttpService service = HttpService();
+
+  @override
+  void initState() {
+    super.initState();
+    service = HttpService();
+    initialize();
+  }
+
+  Future<void> initialize() async {
+    movies = await service.getPopularMovies() as List<Movie>;
+    setState(() {
+      moviesCount = movies.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Movies"),
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: moviesCount,
+        itemBuilder: (context, position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              leading: Image(
+                  image: NetworkImage(movies[position].posterPath ??
+                      "https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg")),
+              title: Text(movies[position].title),
+              subtitle:
+                  Text('Rating: ${movies[position].voteAverage.toString()}'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => MovieDetail(movies[position]),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
